8cbc7f1ccabd75da176b98d62d7a8fd83987844f
rahul <rahulsud48@gmail.com>
2021-07-03 12:01:36 +0000

    uptime added

--- src/format.cpp
+++ src/format.cpp
@@ -8,4 +8,10 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+    int hr, min, sec;
+    hr = seconds/3600;
+    min = (seconds%3600)/60;
+    sec = (seconds%3600)%60;
+    return std::to_string(hr) + ":" + std::to_string(min) + ":" + std::to_string(sec);
+}
